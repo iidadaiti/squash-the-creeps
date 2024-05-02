c4aa28771e2e9f003d850cb59c16075e9f545980
Daiti iida <iidadaiti.yattaki@gmail.com>
2024-05-02 13:50:57 +0900

    update mob script

--- components/mob/mob.gd
+++ components/mob/mob.gd
@@ -1,17 +1,17 @@
 extends CharacterBody3D
 
 # Minimum speed of the mob in meters per second.
-@export var min_speed = 10
+@export var min_speed := 10.0
 # Maximum speed of the mob in meters per second.
-@export var max_speed = 18
+@export var max_speed := 18.0
 
 
-func _physics_process(_delta):
+func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 
 # This function will be called from the Main scene.
-func initialize(start_position, player_position):
+func initialize(start_position: Vector3, player_position: Vector3) -> void:
 	# We position the mob by placing it at start_position
 	# and rotate it towards player_position, so it looks at the player.
 	look_at_from_position(start_position, player_position, Vector3.UP)
@@ -19,8 +19,11 @@ func initialize(start_position, player_position):
 	# so that it doesn't move directly towards the player.
 	rotate_y(randf_range(-PI / 4, PI / 4))
 
+	# reset height
+	position.y = 0
+
 	# We calculate a random speed (integer)
-	var random_speed = randi_range(min_speed, max_speed)
+	var random_speed = randf_range(min_speed, max_speed)
 	# We calculate a forward velocity that represents the speed.
 	velocity = Vector3.FORWARD * random_speed
 	# We then rotate the velocity vector based on the mob's Y rotation
